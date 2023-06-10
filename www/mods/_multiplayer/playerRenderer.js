b5dbdbb0d182bab9c8d31dc818adb0347c9a5b12
MATTIE_FM <mmfuller22@gmail.com>
2023-06-10 12:40:57 -0600

    optimisation

--- www/mods/_multiplayer/playerRenderer.js
+++ www/mods/_multiplayer/playerRenderer.js
@@ -99,6 +99,9 @@ MATTIE.multiplayer.renderer.overrideProcessMove = function (){
                 obj.move = {};
                 obj.move.command = command.code;
                 obj.move.dir4 = Input.dir4;
+                if(command.code != 29){//either standing still or speed change I think?
+
+                }
                 netController.sendHost(obj);
             }
         }
