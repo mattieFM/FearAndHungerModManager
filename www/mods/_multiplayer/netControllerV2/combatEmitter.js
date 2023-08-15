ec81ba9be2db9eb7bdaaa3a5191eefbc8f1ee3ce
MATTIE_FM <mmfuller22@gmail.com>
2023-08-15 09:46:51 -0600

    multicmbat v1.2

--- www/mods/_multiplayer/netControllerV2/combatEmitter.js
+++ www/mods/_multiplayer/netControllerV2/combatEmitter.js
@@ -5,11 +5,16 @@ MATTIE.multiplayer.enemyEmitter = {};
 MATTIE.multiplayer.currentBattleEnemy = MATTIE.multiplayer.currentBattleEnemy || {};
 
 MATTIE.BattleManagerStartTurn = BattleManager.startTurn;
+
+/** log info with the proper conditionals */
 function BattleLog(str) {
     if(MATTIE.multiplayer.devTools.inBattleLogger) console.info(str);
 }
 
-
+/** exit the ready state, returning to the beginning of the input phase 
+ * TODO: instead of returning to the start of the input phase return to the last input so that players can easily correct mistakes without having to...
+ * 
+*/
 BattleManager.unready = function(){
     BattleManager.startInput();
     this._phase = 'input';
@@ -17,6 +22,7 @@ BattleManager.unready = function(){
     MATTIE.multiplayer.BattleController.emitUnreadyEvent();
 }
 
+/** enter the ready state */
 BattleManager.ready = function(){
     this._phase = 'ready';
     
@@ -24,8 +30,25 @@ BattleManager.ready = function(){
     MATTIE.multiplayer.BattleController.emitReadyEvent();
 }
 
+/** check if a battler exists in the local party */
+Game_Battler.prototype.battlerInParty = function(){
+    return $gameParty.battleMembers().contains(this);
+}
+
+/** finds all currently inputted actions for the local party and returns an array of them */
+BattleManager.getAllPlayerActions = function () {
+    let arr = [];
+    this.makeActionOrders();
+    this._actionBattlers.forEach(battler => {
+        if (battler.battlerInParty() && battler.isAlive())
+        arr.push(battler.currentAction());
+    });
+    console.log(arr);
+}
+/** start the combat round */
 BattleManager.startTurn = function() {
     if(MATTIE.multiplayer.currentBattleEvent.totalCombatants() == 1){ //if solo, start next phase
+        this.getAllPlayerActions();
         MATTIE.BattleManagerStartTurn.call(this);
     }else{ //if the player is fighting with allies enter "ready" state
         this.ready();
@@ -33,10 +56,12 @@ BattleManager.startTurn = function() {
     
   };
 
+  /** check that all combatants on this event are ready */
   BattleManager.checkAllPlayersReady = function(){
     return MATTIE.multiplayer.currentBattleEvent.allReady();
   }
 
+  //override YANFly's update function to have the ready state aswell
   BattleManager.update = function() {
     if (!this.isBusy() && !this.updateEvent()) {
         switch (this._phase) {
@@ -48,13 +73,14 @@ BattleManager.startTurn = function() {
             //allow 'un-reading' to go back to previous state.
             //and if all players are ready proceed to turn state.
             if(this.checkAllPlayersReady()){
+                this.getAllPlayerActions();
                  MATTIE.BattleManagerStartTurn.call(this);
-        }
+            }
             
             break;
         case 'turn':
             this.updateTurn();
-            console.log("update turn case")
+            console.log("update turn case:\n" + this._actionList)
             break;
         case 'action':
             this.updateAction();
