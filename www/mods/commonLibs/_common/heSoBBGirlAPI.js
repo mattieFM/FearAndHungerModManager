6998495ec2ed93c34b35b53a28a143721cbd9dd3
MATTIE_FM <mmfuller22@gmail.com>
2023-09-15 23:11:47 -0600

    he so bbgirl

--- www/mods/commonLibs/_common/heSoBBGirlAPI.js
+++ www/mods/commonLibs/_common/heSoBBGirlAPI.js
@@ -5,14 +5,17 @@ MATTIE.bbgirlAPI = MATTIE.bbgirlAPI || {};
 
 var Imported = Imported || {};
 
+/**
+ * @description bbgirl slay queen. Girl boss, pussy boss, girlqueen 
+ */
 MATTIE.bbgirlAPI.yassify = function(){
     console.log("yassed")
     let yassssifyyDict = {
         "Actor1": {
             0:"portrarL_cahara",
-            2:"portraitR_darce",
-            3:"portraitR_girl",
             6:"portraitL_enki",
+            2:"portraitR_darce", 
+            3:"portraitR_girl",
             7:"portraitL_ragn"
         },
         "Actor2":{
@@ -21,11 +24,6 @@ MATTIE.bbgirlAPI.yassify = function(){
         },
         "Actor3":{
             0:"portraitL_nashrah",
-            2:"portraitR_darce",
-            3:"portraitR_girl",
-            6:"portraitL_enki",
-            7:"portraitL_ragn"
-            
         }
     }
     Window_MenuStatus.prototype.drawFace = function(faceName, faceIndex, x, y, width, height) {
@@ -39,8 +37,9 @@ MATTIE.bbgirlAPI.yassify = function(){
         keys.forEach(key => {
             if(faceName.includes(key)){
                 let portraitKeys = Object.keys(yassssifyyDict[key]);
-                if(portraitKeys.includes(faceIndex)){
-                    bustName = yassssifyyDict[key][portraitKeys[faceIndex]];
+                let portraits = yassssifyyDict[key];
+                if(portraitKeys.includes(faceIndex.toString())){
+                    bustName = portraits[faceIndex.toString()];
                 }
             }
         });
