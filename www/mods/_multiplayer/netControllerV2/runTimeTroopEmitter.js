370e516bdb827af37763387e0f20b566b7af1a4d
MATTIE_FM <mmfuller22@gmail.com>
2023-09-16 21:03:06 -0600

    remove multi from main

--- www/mods/_multiplayer/netControllerV2/runTimeTroopEmitter.js
+++
@@ -1,8 +0,0 @@
-var MATTIE = MATTIE || {};
-MATTIE.multiplayer = MATTIE.multiplayer ||  {}
-MATTIE.multiplayer.runTimeTroopEmitter = MATTIE.multiplayer.runTimeTroopEmitter ||  {}
-MATTIE.multiplayer.runTimeTroopEmitter.spawn = MATTIE.troopAPI.runtimeTroop.prototype.spawn;
-MATTIE.troopAPI.runtimeTroop.prototype.spawn = function(ignore=false){
-    MATTIE.multiplayer.runTimeTroopEmitter.spawn.call(this)
-    if(!ignore) MATTIE.multiplayer.getCurrentNetController().emitRuntimeTroopEvent(this._troopId);
-}
