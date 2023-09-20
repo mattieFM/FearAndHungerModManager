f24c76e1d7c6ed4bc15c61661c1ff343e6c78d67
MATTIE_FM <mmfuller22@gmail.com>
2023-09-19 18:31:00 -0600

    pvp

---
+++ www/mods/_multiplayer/netControllerV2/pvpHandler.js
@@ -0,0 +1,16 @@
+var MATTIE = MATTIE || {};
+MATTIE.multiplayer = MATTIE.multiplayer || {};
+MATTIE.multiplayer.pvp = MATTIE.multiplayer.pvp || {};
+
+MATTIE.multiplayer.pvp.PvpController = function () {
+    throw new Error('This is a static class');
+}
+
+MATTIE.multiplayer.pvp.PvpController.setupCombat = function(ids){
+    
+}
+
+
+
+
+
