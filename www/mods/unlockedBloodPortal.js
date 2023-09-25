e672c86250bf15052df4ff736a8e381a52c9b2ad
MATTIE_FM <mmfuller22@gmail.com>
2023-09-25 09:14:51 -0600

    proper defaults for keybinds

--- www/mods/unlockedBloodPortal.js
+++ www/mods/unlockedBloodPortal.js
@@ -10,7 +10,7 @@ var MATTIE = MATTIE || {};
 (()=>{
     Input.addKeyBind('', ()=>{
         $gameTemp.reserveCommonEvent(152);
-    }, "Blood Portal", 1);
+    }, "Blood Portal", 1, "b", "b");
 
     DataManager.changeActivationCondition(MATTIE.static.skills.bloodportal,2);
     DataManager.clearEffects(MATTIE.static.skills.bloodportal);
