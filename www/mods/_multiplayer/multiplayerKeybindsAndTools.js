4ab6b58acae2d2d3dfc6816eaf8b030a8f14fc34
MATTIE_FM <mmfuller22@gmail.com>
2023-09-26 03:27:10 -0600

    surround teleport with try catch

--- www/mods/_multiplayer/multiplayerKeybindsAndTools.js
+++ www/mods/_multiplayer/multiplayerKeybindsAndTools.js
@@ -6,28 +6,33 @@ MATTIE.multiplayer.keybinds.currentIndex = 0;
 
 
 Input.addKeyBind('n', ()=>{
-    let netPlayers = MATTIE.multiplayer.getCurrentNetController().netPlayers;
-    let netPlayerIds = Object.keys(netPlayers)
-    
+    try {
+        let netPlayers = MATTIE.multiplayer.getCurrentNetController().netPlayers;
+        let netPlayerIds = Object.keys(netPlayers)
+        
 
-    if(MATTIE.multiplayer.keybinds.currentIndex < netPlayerIds.length-1){ //handle incrementing / looping
-        MATTIE.multiplayer.keybinds.currentIndex++;
-    }else{
-        MATTIE.multiplayer.keybinds.currentIndex = 0
+        if(MATTIE.multiplayer.keybinds.currentIndex < netPlayerIds.length-1){ //handle incrementing / looping
+            MATTIE.multiplayer.keybinds.currentIndex++;
+        }else{
+            MATTIE.multiplayer.keybinds.currentIndex = 0
+        }
+
+        let playerId = netPlayerIds[MATTIE.multiplayer.keybinds.currentIndex];
+
+        console.log(playerId);
+        console.log(netPlayers);
+        let player = netPlayers[playerId];
+        let mapId = player.$gamePlayer._newMapId != 0 ? player.$gamePlayer._newMapId : player.map;
+        let x = player.$gamePlayer.x;
+        let y = player.$gamePlayer.y;
+        $gamePlayer.reserveTransfer(mapId,x,y, 0, 2);
+        setTimeout(() => {
+            $gamePlayer.performTransfer();
+        }, 250);
+    } catch (error) {
+        
     }
-
-    let playerId = netPlayerIds[MATTIE.multiplayer.keybinds.currentIndex];
-
-    console.log(playerId);
-    console.log(netPlayers);
-    let player = netPlayers[playerId];
-    let mapId = player.$gamePlayer._newMapId != 0 ? player.$gamePlayer._newMapId : player.map;
-    let x = player.$gamePlayer.x;
-    let y = player.$gamePlayer.y;
-    $gamePlayer.reserveTransfer(mapId,x,y, 0, 2);
-    setTimeout(() => {
-        $gamePlayer.performTransfer();
-    }, 250);
+    
     
 
 }, "TP", 0)
