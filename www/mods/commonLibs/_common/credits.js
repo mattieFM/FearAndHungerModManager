6c5a3294d66d9a9a4884aacd67ce2c0398075a86
mattieFM <mattieFM@users.noreply.github.com>
2023-09-28 06:29:42 +0000

    deploy: 3e5a31731b16b32eff7eaf87de6f2c14f254bbe8

--- www/mods/commonLibs/_common/credits.js
+++
@@ -1,8 +0,0 @@
-Game_Interpreter.prototype.setupReservedCommonEvent = function () {
-	if ($gameTemp.isCommonEventReserved()) {
-		this.setup($gameTemp.reservedCommonEvent().list);
-		$gameTemp.clearCommonEvent();
-		return true;
-	}
-	return false;
-};
