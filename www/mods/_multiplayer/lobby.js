b88e8e5b4c12e5e50dd015939028a62a50b8bdec
MATTIE_FM <mmfuller22@gmail.com>
2024-01-20 23:09:19 -0700

    updated docs added context for clients when connecting.

--- www/mods/_multiplayer/lobby.js
+++ www/mods/_multiplayer/lobby.js
@@ -21,7 +21,29 @@ MATTIE.scenes.multiplayer.lobby.prototype.constructor = MATTIE.scenes.multiplaye
 MATTIE.scenes.multiplayer.lobby.prototype.create = function () {
 	MATTIE.scenes.multiplayer.base.prototype.create.call(this);
 	this.createWindowLayer();
+
+	this.connected = false;
+
+	setTimeout(() => {
+		if (!this.connected) {
+			this._connectionStatusField.updateText('NOT CONNECTED, Taking longer than expected...');
+		}
+		setTimeout(() => {
+			if (!this.connected) {
+				window.open('./mods/commonLibs/docs/tutorial-connectionHelp.html', '_blank').focus();
+				this._connectionStatusField.updateText('NOT CONNECTED!');
+			}
+		}, 10000);
+	}, 15000);
+
+	MATTIE.multiplayer.clientController.addListener('clientConnectedToHost', () => {
+		this._connectionStatusField.updateText('Connected');
+		this.connected = true;
+		console.log('connected');
+	});
+
 	MATTIE.multiplayer.clientController.open();
+
 	if (!MATTIE.multiplayer.clientController.self) {
 		MATTIE.multiplayer.clientController.self.on('open', () => {
 			this.addPlayerListWindow();
@@ -44,9 +66,19 @@ MATTIE.scenes.multiplayer.lobby.prototype.create = function () {
 		MATTIE.multiplayer.clientController.emit('startGame');
 	}));
 
+	this.addConnectionDisplayWindow();
+
 	this.addWindow(this._optionsWindow);
 	this._optionsWindow.updateWidth(600);
 	this._optionsWindow.updatePlacement(175 + 300 + 10);
+};
+
+MATTIE.scenes.multiplayer.lobby.prototype.addConnectionDisplayWindow = function () {
+	const text = [
+		'Attempting to connect...',
+	];
+	this._connectionStatusField = new MATTIE.windows.TextDisplay((Graphics.boxWidth - 600) / 2 + 100, 0, 600, 100, text);
+	this.addWindow(this._connectionStatusField);
 };
 
 MATTIE.scenes.multiplayer.lobby.prototype.addPlayerListWindow = function () {
