5ca66b87e3ef472b65c9818751006ffb45a5fa06
mattiefm <mmfuller22@gmail.com>
2025-01-24 19:14:48 -0700

    I think all the decryption issue should be fixed now for termina, time to test on funger 1

--- www/mods/commonLibs/_common/compatibility.js
+++ www/mods/commonLibs/_common/compatibility.js
@@ -83,9 +83,10 @@ Bitmap.prototype.compatabilityLoad = function (url, force = false) {
 
 		rpgmakerWantsToDecrypt = !Decrypter.checkImgIgnore(url) && Decrypter.hasEncryptedImages;
 		modmanagerWantsToDecrypt = fs.existsSync(rpgMVPUrl) && !MATTIE.compat.pauseDecrypt;
+		cannotUseEncrypted = !fs.existsSync(rpgMVPUrl) && fs.existsSync(pngUrl)
 
 		console.log(`modmanger want to decrypt:${modmanagerWantsToDecrypt}\nrpgmakerwantstodecrypt${rpgmakerWantsToDecrypt}`);
-		if ((modmanagerWantsToDecrypt && rpgmakerWantsToDecrypt) || force || (fs.existsSync(rpgMVPUrl) && !fs.existsSync(pngUrl))) {
+		if (((rpgmakerWantsToDecrypt) || force || (fs.existsSync(rpgMVPUrl) && !fs.existsSync(pngUrl))) && !MATTIE.compat.pauseDecrypt && !cannotUseEncrypted) {
 			if (Utils.isNwjs()) {
 				this._loadingState = 'decrypting';
 				Decrypter.decryptImg(pngUrl, this);
